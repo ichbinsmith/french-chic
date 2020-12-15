bf3664cf1108821884359bdc27ab85689de301c6
Smith Djamoura <smith-etchanm.djamoura@etu.unice.fr>
2020-12-15 20:31:20 +0100

    Sprint 3 OK

--- src/main/java/views/ClientAddition.java
+++ src/main/java/views/ClientAddition.java
@@ -32,6 +32,51 @@ public class ClientAddition extends JFrame {
         int largeur = 30;
 
 
+
+
+        JLabel aLabel = null;
+        aLabel = new JLabel("Nom");
+        aLabel.setSize(120, 20);
+        aLabel.setLocation(150, 100);
+        final JTextField aField;
+        aField = new JTextField();
+        aField.setSize(longueur, largeur);
+        aField.setLocation(250, 95);
+        aField.setSize(100, largeur);
+
+        JLabel bLabel = null;
+        bLabel = new JLabel("Prénom");
+        bLabel.setSize(120, 20);
+        bLabel.setLocation(150, 150);
+        final JTextField bField;
+        bField = new JTextField();
+        bField.setSize(longueur, largeur);
+        bField.setLocation(250, 145);
+        bField.setSize(100, largeur);
+
+
+        JLabel cLabel = null;
+        cLabel = new JLabel("Pseudo");
+        cLabel.setSize(120, 20);
+        cLabel.setLocation(150, 200);
+        final JTextField cField;
+        cField = new JTextField();
+        cField.setSize(longueur, largeur);
+        cField.setLocation(250, 195);
+        cField.setSize(100, largeur);
+
+
+        JLabel dLabel = null;
+        dLabel = new JLabel("Mot de passe");
+        dLabel.setSize(120, 20);
+        dLabel.setLocation(150, 250);
+        final JPasswordField dField;
+        dField = new JPasswordField();
+        dField.setSize(longueur, largeur);
+        dField.setLocation(250, 245);
+        dField.setSize(100, largeur);
+
+
         JLabel errorMessage = new JLabel("");
         errorMessage.setLocation(250,355);
         errorMessage.setForeground(Color.RED);
@@ -46,9 +91,12 @@ public class ClientAddition extends JFrame {
             @Override
             public void actionPerformed(ActionEvent arg0) {
                 Session session = new Session();
-                if(session.processClientAddition("add","add","add","add")){
+                if(session.processClientAddition(aField.getText(),bField.getText(),cField.getText(),String.valueOf(dField.getPassword()))){
                     errorMessage.setText("Client ajouté avec succes.");
-                    ajouterClient.setEnabled(false);
+                    aField.setText("");
+                    bField.setText("");
+                    cField.setText("");
+                    dField.setText("");
                 }
                 else {
                     errorMessage.setText("Ajout Impossible.");
@@ -59,5 +107,14 @@ public class ClientAddition extends JFrame {
 
         this.add(errorMessage);
         this.add(ajouterClient);
+
+        this.add(aLabel);
+        this.add(aField);
+        this.add(bLabel);
+        this.add(bField);
+        this.add(cLabel);
+        this.add(cField);
+        this.add(dLabel);
+        this.add(dField);
     }
 }
